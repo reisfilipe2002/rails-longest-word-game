cc714d404cf31901fc80b2b70a831428c48cd2ce
filipe reis <reisfilipe102@gmail.com>
2023-05-23 09:17:38 -0300

    Created the game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    while @letters.size != 10
+      new_letter = ((('A'..'Z').to_a).sample)
+      @letters << new_letter
+
+
+    end
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
